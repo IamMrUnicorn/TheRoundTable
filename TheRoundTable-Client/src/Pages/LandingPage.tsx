cac170cfbcafaa702f38edf10866a8ea224a21d1
jason <quintanajason4004@gmail.com>
2023-08-02 20:11:03 -0400

    updated the default theme, and updated the turn button colors, also finalized the design on the landing page

--- TheRoundTable-Client/src/Pages/LandingPage.tsx
+++ TheRoundTable-Client/src/Pages/LandingPage.tsx
@@ -10,7 +10,7 @@ const LandingPage: FC = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [typingSpeed, setTypingSpeed] = useState(150);
 
-  const phrases = ["host your session", "play dnd", "manage your party", "hang out with friends"];
+  const phrases = ["play dnd", "hang out with friends", "store characters", "track your session", "manage your party"];
 
   useEffect(() => {
     const handleTyping = () => {
@@ -23,7 +23,7 @@ const LandingPage: FC = () => {
           : fullPhrase.substring(0, typingText.length + 1)
       );
 
-      setTypingSpeed(isDeleting ? 30 : 150);
+      setTypingSpeed(isDeleting ? 40 : 150);
 
       if (!isDeleting && typingText === fullPhrase) {
         setTimeout(() => setIsDeleting(true), 500);
@@ -38,30 +38,29 @@ const LandingPage: FC = () => {
   }, [typingText, isDeleting, typingSpeed, loopNum, phrases]);
 
   return (
-    <div data-theme='TheRoundTable' className="h-[95vh] flex flex-col items-center wavebg gap-1">
+    <div data-theme='TheRoundTable' className="h-[95vh] flex flex-col text-white items-center justify-evenly wavebg">
       <h1 className="text-6xl">WELCOME TO THE ROUND TABLE</h1>
       <p className="typewriter"> The best place to <span>{typingText}</span> </p>
-      <h2 className='text-4xl'>the best place to host and manage your dnd sessions</h2>
-      
-      <h3 className='text-3xl'>get started by hosting your own session </h3>
 
-      <div className='flex flex-row justify-center'>
-        <label htmlFor="createRoom_modal" className='btn btn-accent' onClick={()=>{setcreateRoomClicked(true)}}>create a room</label>
-        {createRoomClicked && <CreateRoomModal setPopup={setcreateRoomClicked}/>}
+      <div className='flex flex-col justify-center'>
+        <h3 className='text-3xl'>get started by hosting your own session </h3>
+
+        <div className='flex flex-row justify-center'>
+          <label htmlFor="createRoom_modal" className='btn btn-accent' onClick={() => { setcreateRoomClicked(true) }}>create a room</label>
+          {createRoomClicked && <CreateRoomModal setPopup={setcreateRoomClicked} />}
+        </div>
+
+      </div>
+      <div className='flex flex-col justify-center'>
+
+        <h3 className='text-3xl'>or join your party with the provided invite code</h3>
+
+        <div className='flex flex-row justify-center'>
+          <input className='self-center text-black' type='text' onChange={(e)=>{setCode(e.target.value)}}></input>
+          <a className='btn btn-accent btn-sm' href={`rooms/${code}`}>join room</a>
+        </div>
       </div>
 
-      <h3 className='text-3xl'>or join your party with an invite code</h3>
-
-      <div className='flex flex-row justify-center'>
-        <input className='self-center' type='text'></input>
-        <a className='btn btn-accent btn-sm' href={`rooms/${code}`}>join room</a>
-      </div>
-      
-      <div className='ocean'>
-        <div className='wave'></div>
-        <div className='wave'></div>
-        <div className='wave'></div>
-      </div>
     </div>
   )
 }
