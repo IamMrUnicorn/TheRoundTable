783d433863b354aecbbcd9d5da18ca124654b312
jason <quintanajason4004@gmail.com>
2023-11-07 03:37:00 -0500

    part one of full re write lol

--- TheRoundTable-Client/src/Pages/GamePage.tsx
+++ TheRoundTable-Client/src/Pages/GamePage.tsx
@@ -1,250 +1,168 @@
 import { useContext, useEffect, useState } from 'react'
-import { supabaseContext } from '../utils/supabase';
-import { LoadingPage } from './LoadingPage';
-import DMPage from './DMPage';
-import PlayerPage from './PlayerPage';
+import { supabaseContext } from '../Utils/supabase';
+import {LoadingPage, DMSetupPage, DMPage, PlayerPage, PlayerWaitingPage} from './Index'
+import {getCharacterById, getAllCharacterIdsByPartyId} from '../Utils/SupabaseFns'
+import { characterDataI } from '../Components/CharacterSheet';
 
 
-export interface Character {
-  clerk_user_id: string;
-  alignment: string;
-  background: string;
-  character_inventory: {
-    character_id: number;
-    spells: {
-      cantrips: string[];
-      lvl1: string[];
-      lvl2: string[];
-      lvl3: string[];
-      lvl4: string[];
-      lvl5: string[];
-      lvl6: string[];
-      lvl7: string[];
-      lvl8: string[];
-      lvl9: string[];
-    };
-    weapons: {
-      heavy: string[];
-      light: string[];
-      reach: string[];
-      range: string[];
-      thrown: string[];
-      loading: string[];
-      finesse: string[];
-      special: string[];
-      versatile: string[];
-      twoHanded: string[];
-      magicalWeapons: string[];
-    };
-    inventory: {
-      copper: number;
-      silver: number;
-      gold: number;
-      platinum: number;
-      inventory: string[];
-    };
-  };
-  character_proficiency: {
-    strength: boolean;
-    dexterity: boolean;
-    constitution: boolean;
-    intelligence: boolean;
-    wisdom: boolean;
-    charisma: boolean;
-    athletics: boolean;
-    acrobatics: boolean;
-    sleightOfHand: boolean;
-    intimidation: boolean;
-    performance: boolean;
-    investigation: boolean;
-    animalHandling: boolean;
-    nature: boolean;
-    religion: boolean;
-    history: boolean;
-    insight: boolean;
-    medicine: boolean;
-    perception: boolean;
-    survival: boolean;
-    deception: boolean;
-    stealth: boolean;
-    arcana: boolean;
-    persuasion: boolean;
-  };
-  character_stats: {
-    ac: number;
-    charisma: number;
-    constitution: number;
-    currenthp: number;
-    dexterity: number;
-    feats: string[];
-    initiative: number
-    intelligence: number
-    maxhp: number
-    proficiency: number
-    speed: number
-    spell_dc: number
-    status: string;
-    strength: number;
-    wisdom: number
-  };
-  class: string[];
-  hitdice: string;
-  id: number;
-  image_url: string | null;
-  languages: string[];
-  level: number;
-  name: string;
-  proficiencies: string[];
-  race: string[];
-  subclass: string[];
-}
+export const GamePage = () => {
 
-
-
-export const GamePage = ({ user_id }: { user_id: string }) => {
+  const [userisDM, setUserisDM] = useState<boolean | null>(null);
+  const [characterId, setCharacterId] = useState()
+  const [partyId, setPartyId] = useState<number>()
+  const [isWaiting, setIsWaiting] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [characters, setCharacters] = useState <characterDataI[]>()
+  const [thisUserCharacter, setThisUserCharacter] = useState <characterDataI>()
 
   const supabase = useContext(supabaseContext)
-  const [usersCharacter, setUsersCharacter] = useState<Character | null>(null)
-  const [characters, setCharacters] = useState<Character[] | null>(null)
-  const [userisDM, setUserisDM] = useState<boolean | null>(null);
 
+  interface PartyMember {
+    id: number;
+    party_id: number;
+    user_id: number;
+    is_dm: boolean;
+    character_id: number | null;
+    parties: {
+      name: string;
+    };
+  }
 
-  const isUserTheDm = async (partyName: string) => {
-    let { data, error } = await supabase
-      .from('parties')
-      .select('DM_clerk_id')
-      .eq('name', partyName)
-    if (error) {
-      console.log(error)
-    } else {
-      console.log(user_id, data![0].DM_clerk_id)
-      setUserisDM(user_id === data![0].DM_clerk_id ? true : false)
+  const getAllPartyMemberInfoInRoom = async (partyName: string) => {
+    try {
+      let { data, error } = await supabase
+        .from('party_members')
+        .select(`
+          *,
+          parties (
+            name
+          )
+        `)
+        .eq('parties.name', partyName);
+
+      if (error) throw error;
+      if (data && data.length === 0 || !data) throw new Error("No matching records found.");
+      console.log(data.filter(item => item.parties !== null))
+      return data.filter(item => item.parties !== null);
+
+    } catch (error) {
+      console.error(error);
     }
   }
 
-  const getCharactersFromPartyId = async (partyId: number) => {
-    try {
-      let { data: Character, error } = await supabase
-        .from('characters')
-        .select(`
-          id,
-          clerk_user_id,
-          name,
-          image_url,
-          race,
-          class,
-          subclass,
-          background,
-          alignment,
-          level,
-          hitdice,
-          languages,
-          proficiencies,
-          character_stats:character_stats (
-            *
-          ),
-          character_proficiency:character_proficiency (
-            *
-          ),
-          character_inventory:character_inventory (
-            *
-          )
-        `)
-        .eq('party_id', partyId)
+  const getPartyNameFromURL = () => {
+    return decodeURIComponent(window.location.pathname.slice(7))
+  }
 
-      if (error || !Character) throw error;
-      const transformedCharacters = await Promise.all(Character.map(async (character: any): Promise<Character> => {
-
-        character.character_stats = character.character_stats[0];
-        delete character.character_stats.id;
-        delete character.character_stats.character_id;
-        delete character.character_proficiency.character_id;
-
-        character.race = JSON.parse(character.race)
-        character.class = JSON.parse(character.class)
-        character.subclass = JSON.parse(character.subclass)
-        character.languages = JSON.parse(character.languages)
-        character.proficiencies = JSON.parse(character.proficiencies)
-        character.character_stats.feats = JSON.parse(character.character_stats.feats);
-
-        ['spells', 'weapons', 'inventory'].forEach(key => {
-          character.character_inventory[key] = JSON.parse(character.character_inventory[key]);
-        });
-
-        return character as Character;
-      }));
-
-      setCharacters(transformedCharacters);
-    } catch (error) {
-      console.log(error);
-      return null;
-    }
-  };
-
-  const getRoomIdFromName = async (partyName: string) => {
+  const getRoomStatus = async (partyId: number) => {
     try {
       let { data, error } = await supabase
         .from('parties')
-        .select('id')
-        .eq('name', partyName)
-        .single();
-
-      if (error || !data) throw error;
-
-      return data.id;
+        .select('setup')
+        .eq('id', partyId)
+        .single()
+      if (error || !data) throw error
+      setIsWaiting(!data.setup)
     } catch (error) {
-      console.error(error);
-      return null; // Return null or handle the error as needed
-    }
-  };
-
-  const getRoomStatus = async (partyName : string) => {
-    let { data, error } = await supabase
-      .from('parties')
-      .select('setup')
-      .eq('name', partyName)
-    if (error) {
       console.log(error)
-    } else {
-      if (data![0].setup === false) {
-        window.location.href = `/waiting-room/${partyName}`
-      }
+    }
+  }
+
+  const getUserID = async () => {
+    try {
+      let {data, error} = await supabase
+      .from('users')
+      .select('id')
+      .single()
+      if (!data) return null
+      if (error) throw error
+      return data as {id: number}
+    } catch (error) {
+      console.log(error)
     }
   }
 
 
-  
   useEffect(() => {
-    
-    let partyNameInUrl = decodeURIComponent(window.location.pathname.slice(7));
-    getRoomStatus(partyNameInUrl)
-    
-    isUserTheDm(partyNameInUrl)
-    getRoomIdFromName(partyNameInUrl)
-      .then(roomId => {
-        getCharactersFromPartyId(roomId);
-      })
-      .catch(error => {
-        console.error('An error occurred:', error);
-      });
+    const fetchData = async () => {
+      setIsLoading(true);
 
-    const findUsersCharacterFromParty = (characters: Character[] | null) => {
-      if (!characters) return
-      const thisUsersCharacter = characters.filter((character) => (character.clerk_user_id === user_id))
-      setUsersCharacter(thisUsersCharacter[0])
+      try {
+        const userID = await getUserID()
+
+        const partyMembersInfo = await getAllPartyMemberInfoInRoom(getPartyNameFromURL()) as PartyMember[];
+
+        if (!userID) return null
+
+        const thisUser: PartyMember = partyMembersInfo?.filter((data)=>data.user_id === userID.id)[0]  
+        
+        if (thisUser) {
+          setPartyId(thisUser.party_id);
+          setUserisDM(thisUser.is_dm);
+
+          // Fetch all character IDs for the party
+          const characterIds = await getAllCharacterIdsByPartyId(thisUser.party_id);
+
+          if (characterIds) {
+            // Fetch the data for all characters
+            const characterDataArray: (characterDataI | null)[] = await Promise.all(
+              characterIds.map((id) => getCharacterById(id))
+            );
+
+            // Filter out any nulls if a character wasn't found
+            const charactersData = characterDataArray.filter((character): character is characterDataI => character !== null);
+            setCharacters(charactersData)
+            const thisCharacter = charactersData.filter((character)=> character.creator_id === userID.id)[0]
+            setThisUserCharacter(thisCharacter)
+          }
+
+          // Get the room status for the party
+          await getRoomStatus(thisUser.party_id);
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+  // figure out realtime data subscription
+  const parties = supabase.channel('custom-filter-channel')
+    .on(
+      'postgres_changes',
+      {
+        event: '*',
+        schema: 'public',
+        table: 'parties',
+        filter: 'name=eq.someValue'
+      },
+      (payload) => {
+        console.log('Change received!', payload)
+      }
+    )
+    .subscribe()
+
+
+  if (isLoading) return <LoadingPage />
+
+  if (isWaiting) {
+    if (userisDM === true) {
+      return <DMSetupPage />
+    } else if (userisDM === false) {
+      return <PlayerWaitingPage character_id={characterId}/>
     }
 
-    findUsersCharacterFromParty(characters)
-  }, [])
-
-  console.log(characters)
-
+  }
 
   if (userisDM === true) {
-    return <DMPage party={characters} />
+    return <DMPage party={characters}/>
   } else if (userisDM === false) {
-    return <PlayerPage party={characters} usersCharacter={usersCharacter}/>
+    return <PlayerPage party={characters} usersCharacter={thisUserCharacter}/>
   } else {
-    return <LoadingPage />
+
   }
 }
--- TheRoundTable-Client/src/Pages/PlayerWaitingPage.tsx
+++ TheRoundTable-Client/src/Pages/PlayerWaitingPage.tsx
@@ -1,82 +1,28 @@
 import { Tldraw } from '@tldraw/tldraw';
 import '@tldraw/tldraw/tldraw.css';
 import { useEffect, useState } from 'react';
-import { supabase } from '../utils/supabase';
 
 export interface waitingPageI {
   user_id: string;
   partyName: string;
 }
 
-export const PlayerWaitingPage = ({ user_id, partyName }: waitingPageI) => {
-  const [partyId, setPartyId] = useState<string | null>(null);
+export const PlayerWaitingPage = ({character_id}: {character_id:number | undefined}) => {
   const [userInParty, setUserInParty] = useState<boolean>(false);
   const [hasCharacterInParty, setHasCharacterInParty] = useState<boolean>(false);
 
-  const getRoomsUsersArr = async (partyName: string) => {
-    const { data, error } = await supabase
-      .from('parties')
-      .select('users')
-      .eq('name', partyName);
-    if (error) {
-      console.log(error);
-    } else {
-      // Check if user_id is in the returned array
-      let users:string[] = (data[0].users);
-      console.log(users)
-      if (users && users.includes(user_id)) {
-        setUserInParty(true);
-      }
-    }
-  };
-
-  const getPartyIdFromName = async (partyName: string) => {
-    const { data, error } = await supabase
-      .from('parties')
-      .select('id')
-      .eq('name', partyName)
-      .limit(1);
-    if (error) {
-      console.log(error);
-    } else {
-      // Use the party ID for the next function
-      if (data && data.length > 0) {
-        setPartyId(data[0].id);
-      }
-    }
-  };
-
-  const searchUsersCharactersInParty = async (user_id: string, party_id: string) => {
-    const { data, error } = await supabase
-      .from('characters')
-      .select('id')
-      .eq('party_id', party_id)
-      .eq('clerk_user_id', user_id);
-    if (error) {
-      console.log(error);
-    } else {
-      if (data && data.length > 0) {
-        setHasCharacterInParty(true);
-      }
-    }
-  };
-
-  useEffect(() => {
-    (async () => {
-      await getRoomsUsersArr(partyName);
-      await getPartyIdFromName(partyName);
-      if (partyId) {
-        await searchUsersCharactersInParty(user_id, partyId);
-      }
-    })();
-  }, [partyName, partyId]);
-
+  useEffect(()=>{
+    if (character_id) {
+      setHasCharacterInParty(true)
+      setUserInParty(true)
+    } 
+  })
   return (
     <div className='h-[94vh]'>
       {userInParty && hasCharacterInParty ? (
         <div className="bg-neutral m-2 rounded-2xl h-1/2 w-1/2 mx-auto">
           <Tldraw />
-          <p className='font-accent'>character locked in, waiting on DM to finish setup</p>
+          <p className='font-accent'>waiting on DM to finish setup</p>
         </div>
       ) : userInParty ? (
         <div> Prompt for character selection and if not then invite user to create a character</div>
