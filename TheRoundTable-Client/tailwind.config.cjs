a832d1e90542ed7d0ee5a8b1577f6b716890254b
jason <quintanajason4004@gmail.com>
2024-01-07 23:37:05 -0500

    added new prose utility class

--- TheRoundTable-Client/tailwind.config.cjs
+++ TheRoundTable-Client/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ['./src/**/*.{js,ts,jsx,tsx}'];
-export const plugins = [require("daisyui")];
+export const plugins = [require("@tailwindcss/typography"), require("daisyui")];
 export const daisyui = {
   themes: [
     {
